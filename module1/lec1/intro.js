32714919dc0aaf98d648030331b111e86e1122cb
vinay <vinay01431998@gmail.com>
2021-05-17 19:54:04 +0530

    HoisingPromises

---
+++ module1/lec1/intro.js
@@ -0,0 +1,26 @@
+/*console.log(a);
+console.log(b);
+var a="dnjjk";//hosting hoti hai var se//bina intialise get place
+console.log(a);*/
+
+
+/*console.log(fun);
+fun();
+var fun=function(){
+    console.log("hi this is h\fun")
+}*/
+
+
+console.log("Start");
+var name = "Steve";
+
+
+function getName(name){
+    return name + " Rogers";
+}
+
+var fullName = getName("Steve");
+var newName = getName("Tony");
+
+console.log(fullName);
+console.log(newName); 
